c33cc2e6506f2e4941d687ba31269a365d141845
Srikanth Astaputhra <sastaputhra@enova.com>
2015-02-04 10:49:54 -0600

    added indexes, and explain statemnets before adding index and after adding index

--- yadda/create_yadda_index.sql
+++ yadda/create_yadda_index.sql
@@ -1,6 +1,8 @@
 BEGIN;
   CREATE INDEX beer_id_idx ON yadda.ratings(beer_id);
+  CREATE INDEX style_idx ON yadda.beer_styles(style);
+  CREATE INDEX name_idx ON yadda.beers(name);
   CREATE INDEX berewery_id_idx ON yadda.beers(brewery_id);
   CREATE INDEX style_id_idx ON yadda.beers(style_id);
-  CREATE INDEX created_on_idx ON yadda.beer_styles(created_on);
+  CREATE INDEX created_on_idx ON yadda.ratings(created_on);
 COMMIT;
